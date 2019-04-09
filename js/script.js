5980e8db4d4efa7e9b7d7aa9f9f01bf134b83683
heyderelekberli <HeydarRa@code.edu.az>
2019-04-09 21:36:42 +0400

    completed

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    $(".animatedPart").click(function(){
+        $(".movePart").slideToggle("slow");
+      });
+      $(".animatedPart1").click(function(){
+        $(".movePart1").slideToggle("slow");
+      });
+      $(".animatedPart3").click(function(){
+        $(".movePart3").slideToggle("slow");
+      });
+  
+  $(".btn1").click(function(){
+      $(".content1").css({
+          "color":"navy",
+          "font-size":"30px",
+          "background-image":"url('img/redhead.jpg')",
+          "background-size":"50vh 65vh"
+       } )
+  })
+  $(".btn2").click(function(){
+    $(".content2").css({
+        "color":"red",
+
+        "font-size":"25px",
+        "background-color":"green",
+     
+      
+     } )
+})
+$(".btn3").click(function(){
+    $(".content3").css({
+        "color":"white",
+        "font-size":"25px",
+        "background-color":"black",
+     
+      
+     } )
+})
+  });
